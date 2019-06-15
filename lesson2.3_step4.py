9905e3921e304de3bd0c27fcf175b1768b80a336
amdiag <amdiag@ya.ru>
2019-06-15 23:06:46 +0300

    Create lesson2.3_step4

---
+++ lesson2.3_step4.py
@@ -0,0 +1,9 @@
+from selenium import webdriver
+import math
+
+def calc(x):
+    return str(math.log(abs(12*math.sin(int(x)))))
+
+link = "http://suninjuly.github.io/alert_accept.html"
+browser = webdriver.Chrome()
+browser.get(link)
