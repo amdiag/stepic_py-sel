770e40771df0c56de410f97a66dc07596a587b8c
fkudriak <amdiag@gmail.com>
2019-06-17 08:29:07 +0300

    upd done lesson2.3_step4

--- lesson2.3_step4.py
+++ lesson2.3_step4.py
@@ -7,3 +7,18 @@ def calc(x):
 link = "http://suninjuly.github.io/alert_accept.html"
 browser = webdriver.Chrome()
 browser.get(link)
+
+button = browser.find_element_by_css_selector(".btn.btn-primary").click()
+
+confirm = browser.switch_to.alert
+confirm.accept()
+
+xpathX = "//span[@id='input_value']"
+xpathInput = "//input[@id='answer']"
+xpathCheck = ""
+xpathRadio = ""
+calcValue = calc( browser.find_element_by_xpath( xpathX ).text )
+input = browser.find_element_by_xpath(xpathInput)
+input.send_keys(calcValue)
+
+button = browser.find_element_by_css_selector(".btn.btn-primary").click()
